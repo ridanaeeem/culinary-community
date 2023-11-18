3aaf568c4a9a7248d242d530e678b9e2f18e83f2
KingTingTheGreat <jting@bu.edu>
2023-11-18 14:55:00 -0500

    temporary pages

---
+++ src/app/events/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header";
+
+export default function Home() {
+	return (
+		<>
+			<Header />
+			<main className="flex flex-col items-center justify-between">events</main>
+		</>
+	);
+}
