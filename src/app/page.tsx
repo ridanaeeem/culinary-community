d34ad27fb27596e56e996254900a79d6a715986c
ridanaeeem <ridan@bu.edu>
2023-11-18 13:22:25 -0500

    home page progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Header } from "@/components/header";
+import Image from "next/image";
+import  homeBackground from "@/images/designs/homeBackground.svg";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
-			<main className="flex min-h-screen flex-col items-center justify-between p-24">Cul Com</main>
+			<main className="flex min-h-screen flex-col items-center justify-between">
+				<Image src={homeBackground} alt="hiii" className="w-full"/>
+			</main>
 		</>
 	);
 }
