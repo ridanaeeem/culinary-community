0b23dbb4c0df9550eda4db4a659413c9920d4393
ridanaeeem <ridan@bu.edu>
2023-11-18 18:29:35 -0500

    temp links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import homeBackground from "@/images/designs/homeBackground.svg";
 import { Header } from "@/components/header";
 import { Features } from "@/components/features";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<main className="flex flex-col items-center justify-between">
+				<br></br>
+				<br></br>
+				<br></br>
+				<Link href="/events">Events</Link>
+				<Link href="/recipes">Recipes</Link>
 				<Image src={homeBackground} alt="hiii" className="w-full" />
 				<Features />
 			</main>
