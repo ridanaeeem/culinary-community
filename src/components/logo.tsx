91cb1a67de137cd6f552922aad57388ae501e7ba
KingTingTheGreat <jting@bu.edu>
2023-11-18 15:06:36 -0500

    modern export style

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
 
-export function Logo() {
+export const Logo = () => {
 	return (
 		<Link href="/">
-			<h1 className="text-fern text-4xl font-Inika leading-[28px]">
-				Culinary Community
-			</h1>
+			<h1 className="text-fern text-4xl font-Inika leading-[28px]">Culinary Community</h1>
 		</Link>
 	);
-}
+};
