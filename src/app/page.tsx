fb3c9f8436364b44590aec75462d81ddf38c0973
KingTingTheGreat <jting@bu.edu>
2023-11-18 15:09:56 -0500

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
+import Image from "next/image";
+import homeBackground from "@/images/designs/homeBackground.svg";
 import { Header } from "@/components/header";
 import { Features } from "@/components/features";
-import Image from "next/image";
-import  homeBackground from "@/images/designs/homeBackground.svg";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<main className="flex flex-col items-center justify-between">
-				<Image src={homeBackground} alt="hiii" className="w-full"/>
+				<Image src={homeBackground} alt="hiii" className="w-full" />
 				<Features />
 			</main>
 		</>
