06e3f249c7b16fd60891b406326f713c31d1134b
ridanaeeem <ridan@bu.edu>
2023-11-19 10:03:26 -0500

    image fix

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -15,16 +15,23 @@ export default async function Events() {
 		return (
 			<>
 				<Header />
+				<br></br>
+				<br></br>
+				<br></br>
 
-				<div className="brightness-50 absolute">
+				<div className="brightness-50 width-100% z-n10">
 					<Image src={homeBackground}></Image>
 				</div>
-				<br></br>
-				<br></br>
-				<br></br>
-				<div className="absolute text-white">
-					<h1 className="text-7xl">Find Local Events</h1>
-					<p className="text-2xl">Learn new skills with people right in your own community</p>
+				<div className="absolute top-0 text-white text-center px-60 m-40 z-10">
+					<h1 className="text-7xl shadow-2xl">Find Local Events</h1>
+					<p className="text-4xl my-14 shadow-2xl">
+						Learn new skills and meet new people right in your own community.
+					</p>
+					<Link href="/" className="text-4xl shadow-2xl">
+						We believe that there should be no barriers to food access and education. Click here for our
+						options for those who might need financial assistance, or to find out how you can share your
+						resources to help others in your own community.
+					</Link>
 				</div>
 				<div className="flex justify-center">
 					<EventPreviews events={data} limit={Infinity} />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,16 +8,16 @@ export const Header = () => {
 				<Logo />
 			</div>
 			<div className="flex flex-row justify-around items-center w-1/3">
-				<Link href="/events" className="text-3xl">
+				<Link href="/events" className="text-3xl px-2">
 					Events
 				</Link>
-				<Link href="/recipes" className="text-3xl">
+				<Link href="/recipes" className="text-3xl px-2">
 					Recipes
 				</Link>
-				<Link href="/community" className="text-3xl">
+				<Link href="/community" className="text-3xl px-2">
 					Community
 				</Link>
-				<Link href="/about" className="text-3xl">
+				<Link href="/about" className="text-3xl px-2">
 					About
 				</Link>
 			</div>
