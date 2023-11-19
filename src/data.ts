1adc1363a47a89fa702e150e83294f5aa139cc9c
KingTingTheGreat <jting@bu.edu>
2023-11-19 01:18:20 -0500

    work in progress

--- src/data.ts
+++ src/data.ts
@@ -11,60 +11,85 @@ import { Event, Recipe } from "@/types";
 
 import { FetchEvents } from "./events/events";
 
-const clientPromise = require("@/lib/mongodb");
+// const clientPromise = require("@/lib/mongodb");
 
-// export const Events: Event[] = [];
-const test = async ({ req, res }: { req: any; res: any }) => {
-	try {
-		const client = await clientPromise;
-		const db = client.db("CCDatabase");
+// // export const Events: Event[] = [];
+// const test = async ({ req, res }: { req: any; res: any }) => {
+// 	try {
+// 		const client = await clientPromise;
+// 		const db = client.db("CCDatabase");
 
-		const fetch = await db.collection("Events").find({}).toArray();
+// 		const fetch = await db.collection("Events").find({}).toArray();
 
-		// convert fetch to an array of Events
-		const events: Event[] = [];
+// 		// convert fetch to an array of Events
+// 		const events: Event[] = [];
 
-		fetch.map((event: Event) => {
-			try {
-				events.push({
-					_id: event._id,
-					title: event.title,
-					description: event.description,
-					price: event.price,
-					date: event.date,
-					time: event.date,
-					location: event.location,
-					hosts: event.hosts,
-					capacity: event.capacity,
-					attendees: event.attendees,
-					tags: event.tags,
-					preview: event.preview,
-					images: event.images,
-				});
-			} catch (e) {
-				console.error(e);
-			}
-		});
-		return events;
-	} catch (e) {
-		console.error(e);
-		return null;
-	}
-};
+// 		fetch.map((event: Event) => {
+// 			try {
+// 				events.push({
+// 					_id: event._id,
+// 					title: event.title,
+// 					description: event.description,
+// 					price: event.price,
+// 					date: event.date,
+// 					time: event.date,
+// 					location: event.location,
+// 					hosts: event.hosts,
+// 					capacity: event.capacity,
+// 					attendees: event.attendees,
+// 					tags: event.tags,
+// 					preview: event.preview,
+// 					images: event.images,
+// 				});
+// 			} catch (e) {
+// 				console.error(e);
+// 			}
+// 		});
+// 		return events;
+// 	} catch (e) {
+// 		console.error(e);
+// 		return null;
+// 	}
+// };
+
+import { MongoClient } from "mongodb";
+import { get } from "http";
+
+const uri = process.env.MONGODB_URI!;
+
+let client: MongoClient;
+let database;
+
+async function connectDatabase() {
+	client = new MongoClient(uri);
+
+	database = client.db("CCDatabase");
+
+	return database;
+}
+
+export async function getAllEvents() {
+	const db = await connectDatabase();
+	const events = await db.collection("Events").find({}).toArray();
+
+	return events;
+}
 
 // list of events and lookup tables
-export const Events: Event[] | null = await test({ req: null, res: null });
+
+const l = getAllEvents();
+
+export const Events: Event[] = [];
 
 export const EventsByTitle: { [name: string]: Event } = {};
 export const EventsById: { [key: string]: Event } = {};
-if (Events) {
-	for (const event of Events) {
-		EventsByTitle[event.title.replace(/\s+/g, "-").toLowerCase()] = event;
-	}
-	for (const event of Events) {
-		EventsById[event.id] = event;
-	}
+for (const event of Events) {
+	EventsByTitle[event.title.replace(/\s+/g, "-").toLowerCase()] = event;
 }
+for (const event of Events) {
+	EventsById[event._id] = event;
+}
+
 // list of recipes and lookup tables
 export const Recipes: Recipe[] = [Koshari, ScallionPancake, ShrimpScampi];
 
