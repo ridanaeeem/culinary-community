e87c174bd5655144001635423a6417c66700bf31
KingTingTheGreat <jting@bu.edu>
2023-11-19 04:48:49 -0500

    rework of events page using database

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,53 +1,18 @@
-// import { Header } from "@/components/header";
-// import { EventPreviews } from "@/components/event-previews";
+import { EventPreviews } from "@/components/event-previews";
+const axios = require("axios");
 
-// export default function Recipes() {
-// 	return (
-// 		<>
-// 			<Header />
-// 			<main className="content-center">
-// 				<EventPreviews limit={Infinity} />
-// 			</main>
-// 		</>
-// 	);
-// }
-"use client";
-import { useEffect, useState } from "react";
-import { Event } from "@/types";
-import Link from "next/link";
-
-interface EventsProps {
-	events: Array<Event>;
+async function fetchData() {
+	try {
+		const response = await axios.get("http://localhost:3000/api/events"); // replace with deployment endpoint
+		const data = response.data;
+		if (!(data instanceof Array)) {
+			throw new Error("Response data is not an array");
+		}
+		return <EventPreviews events={data} limit={Infinity} />;
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		throw error;
+	}
 }
 
-function Event({ events }: EventsProps) {
-	// Use state to manage the events
-	const [eventList, setEventList] = useState(events);
-
-	// Fetch data on the client side
-	useEffect(() => {
-		const fetchData = async () => {
-			const res = await fetch(process.env.API_URL as string);
-			const data = await res.json();
-			setEventList(data);
-		};
-
-		fetchData();
-	}, []);
-	if (!eventList) return <div>Loading...</div>;
-	return (
-		<div>
-			<h1>Event List</h1>
-			<h2>Click On Event to see it individually</h2>
-			{eventList.map((evt) => (
-				<div key={evt._id}>
-					<Link href={`/events/${evt._id}`}>
-						<h3 style={{ cursor: "pointer" }}>{evt.title}</h3>
-					</Link>
-				</div>
-			))}
-		</div>
-	);
-}
-
-export default Event;
+export default fetchData;
