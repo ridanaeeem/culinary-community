822e51b4534ee1db42eff6a5a9ef7642838e0249
KingTingTheGreat <jting@bu.edu>
2023-11-19 07:39:13 -0500

    new home page for failed loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,30 @@ export default async function Home() {
 		);
 	} catch (error) {
 		console.error("Error fetching data:", error);
-		return <div>Error loading page</div>;
+		return (
+			<>
+				<Header />
+				<main className="flex flex-col items-center justify-between">
+					<br></br>
+					<br></br>
+					<br></br>
+					<div>
+						<div className="mx-10 text-center absolute left-20 top-32 w-20%">
+							<p className="my-10 text-5xl">Connect with your community through food</p>
+							<button className="m-10 px-5 py-2 text-4xl rounded-full bg-[#156E0E] text-white shadow-xl">
+								Sign Up
+							</button>
+							<button className="px-7 py-2 text-4xl rounded-full bg-[#156E0E] text-white shadow-xl">
+								Log In
+							</button>
+						</div>
+					</div>
+					<Image src={homeBackground} alt="hiii" className="w-full" />
+					<Link href="/events">Events</Link>
+					<Link href="/recipes">Recipes</Link>
+					<Features />
+				</main>
+			</>
+		);
 	}
 }
