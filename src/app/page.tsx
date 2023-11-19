e5dd1bef80b8b93e0f28feccfb4371767649f32c
ridanaeeem <ridan@bu.edu>
2023-11-18 20:16:37 -0500

    home page edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,20 @@ export default function Home() {
 				<br></br>
 				<br></br>
 				<br></br>
+				<div>
+					<div className="mx-10 text-center absolute left-20 top-32 w-20%">
+						<p className="my-10 text-5xl">Connect with your community through food</p>
+						<button className="m-10 px-5 py-2 text-4xl rounded-full bg-[#156E0E] text-white shadow-xl">
+							Sign Up
+						</button>
+						<button className="px-7 py-2 text-4xl rounded-full bg-[#156E0E] text-white shadow-xl">
+							Log In
+						</button>
+					</div>
+				</div>
+				<Image src={homeBackground} alt="hiii" className="w-full" />
 				<Link href="/events">Events</Link>
 				<Link href="/recipes">Recipes</Link>
-				<Image src={homeBackground} alt="hiii" className="w-full" />
 				<Features />
 			</main>
 		</>
